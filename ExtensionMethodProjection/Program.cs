735dda5dc07e34ac69a6a047abd138b898d71eef
Luke McGregor <luke@mcgregor.org.nz>
2016-10-05 13:47:13 +1300

    Added another level of depth to the projection to demonstrate replacement failure

--- ExtensionMethodProjection/Program.cs
+++ ExtensionMethodProjection/Program.cs
@@ -1,82 +1,109 @@
-﻿using ExtensionMethodProjection.StaticVoid.ToolSite.Site.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExtensionMethodProjection
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (var ctx = new MyContext())
-            {
-                var b = ctx.People.AsExtendable().ToViewModels().ToArray();
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var ctx = new MyContext())
+			{
+				var b = ctx.People.AsExtendable().ToViewModels().ToArray();
+			}
+		}
+	}
 
-    public class MyContext : DbContext
-    {
-        public MyContext() : base("ExtensionMethodProjection") { }
-        public DbSet<Person> People { get; set; }
-        public DbSet<Profile> Profile { get; set; }
+	public class MyContext : DbContext
+	{
+		public MyContext() : base("ExtensionMethodProjection") { }
+		public DbSet<Person> People { get; set; }
+		public DbSet<Profile> Profile { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Person>().HasMany(x => x.Friends).WithMany();
-            modelBuilder.Entity<Person>().HasRequired(x => x.Me).WithMany().HasForeignKey(x => x.ProfileId).WillCascadeOnDelete(false);
-            base.OnModelCreating(modelBuilder);
-        }
-    }
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Person>().HasMany(x => x.Friends).WithMany();
+			modelBuilder.Entity<Profile>().HasMany(x => x.SomethingElses).WithMany();
+			modelBuilder.Entity<Person>().HasRequired(x => x.Me).WithMany().HasForeignKey(x => x.ProfileId).WillCascadeOnDelete(false);
+			base.OnModelCreating(modelBuilder);
+		}
+	}
 
-    public class Person
-    {
-        public int Id { get; set; }
-        public int ProfileId { get; set; }
-        public Profile Me { get; set; }
-        public ICollection<Profile> Friends { get; set; }
-    }
-    public class Profile
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+	public class Person
+	{
+		public int Id { get; set; }
+		public int ProfileId { get; set; }
+		public Profile Me { get; set; }
+		public ICollection<Profile> Friends { get; set; }
+	}
+	public class Profile
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public ICollection<SomethingElse> SomethingElses { get; set; }
+	}
 
+	public class SomethingElse
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
 
-    public class PersonModel
-    {
-        public ProfileModel Me { get; set; }
-        public IEnumerable<ProfileModel> Friends { get; set; }
-    }
-    public class ProfileModel
-    {
-        public string Name { get; set; }
-    }
-    public static class Extensions
-    {
-        [ExpandableMethod]
-        public static IQueryable<PersonModel> ToViewModels(this IQueryable<Person> entities)
-        {
-            return entities.Select(x => new PersonModel
-            {
-                Me = x.Me.ToViewModel(), //this method cannot be translated into a store expression
-                Friends = x.Friends.AsQueryable().ToViewModels() //works fine with some magic (tm)
-            });
-        }
+	public class PersonModel
+	{
+		public ProfileModel Me { get; set; }
+		public IEnumerable<ProfileModel> Friends { get; set; }
+	}
+	public class ProfileModel
+	{
+		public string Name { get; set; }
+		public IEnumerable<SomethingElseModel> SomethingElses { get; set; }
+	}
+	public class SomethingElseModel
+	{
+		public string Name { get; set; }
+	}
+	public static class Extensions
+	{
+		[ExpandableMethod]
+		public static IQueryable<PersonModel> ToViewModels(this IQueryable<Person> entities)
+		{
+			return entities.Select(x => new PersonModel
+			{
+				Me = x.Me.ToViewModel(), //this method cannot be translated into a store expression
+				Friends = x.Friends.AsQueryable().ToViewModels() //works fine with some magic (tm)
+			});
+		}
 
-        [ExpandableMethod]
-        public static IQueryable<ProfileModel> ToViewModels(this IQueryable<Profile> entities)
-        {
-            return entities.Select(x => new ProfileModel { Name = x.Name });
-        }
-        
-        public static ProfileModel ToViewModel(this Profile entity)
-        {
-            return new ProfileModel { Name = entity.Name };
-        }
-    }
+		[ExpandableMethod]
+		public static IQueryable<ProfileModel> ToViewModels(this IQueryable<Profile> entities)
+		{
+			return entities.Select(x => new ProfileModel { Name = x.Name });
+		}
+
+		[ExpandableMethod]
+		public static IQueryable<SomethingElseModel> ToViewModels(this IQueryable<SomethingElse> entities)
+		{
+			return entities.Select(x => new SomethingElseModel { Name = x.Name });
+		}
+
+		public static Expression<Func<Profile, ProfileModel>> ToPublicViewModelExpression()
+		{
+			return entity => new ProfileModel
+			{
+				Name = entity.Name,
+				SomethingElses = entity.SomethingElses.AsQueryable().ToViewModels() //this depth breaks the parameter replacement
+			};
+		}
+		[ReplaceInExpressionTree(MethodName = nameof(ToPublicViewModelExpression))]
+		public static ProfileModel ToViewModel(this Profile entity)
+		{
+			return ToPublicViewModelExpression().Compile()(entity);
+		}
+	}
 }
